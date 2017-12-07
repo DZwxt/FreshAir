02d4b30d82e28517d422e5363f82b558c98cda9f
DZwxt <18331446114@163.com>
2017-12-07 22:18:14 +0800

    生产者消费者 queue 方式，点对点，一次消息只能有一个消费者

--- src/main/java/activeMQ/Comsumer.java
+++ src/main/java/activeMQ/Comsumer.java
@@ -1,5 +1,44 @@
 package activeMQ;
 
+import java.util.Enumeration;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MapMessage;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class Comsumer {
+	
+	public static void main(String[] args) throws Exception {
+		ConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		Connection connection = factory.createConnection();
+		connection.start();
+		
+		Enumeration names = connection.getMetaData().getJMSXPropertyNames();
+		while (names.hasMoreElements()) {
+			String object = (String) names.nextElement();
+			System.out.println(object);
+		}
+		
+		Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue("my-queue");
+		
+		MessageConsumer consumer = session.createConsumer(destination);
+		
+		int i = 0;
+		while (i<3) {
+			i++;
+			MapMessage message = (MapMessage) consumer.receive();
+			session.commit();
+			System.out.println("接收到消息:"+message.getBoolean("message"+i)+"propert:"+message.getBooleanProperty("property"+i));
+			
+		}
+		session.close();
+		connection.close();
+	}
 
 }
--- src/main/java/activeMQ/Producter.java
+++ src/main/java/activeMQ/Producter.java
@@ -4,7 +4,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
@@ -14,56 +16,78 @@ import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class Producter {
-	private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
-	private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
-	private static final String BROKEN_UEL = ActiveMQConnection.DEFAULT_BROKER_URL;
+//	private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;
+//	private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;
+//	private static final String BROKEN_UEL = ActiveMQConnection.DEFAULT_BROKER_URL;
+//	
+//	AtomicInteger count = new AtomicInteger(0);
+//	
+//	ConnectionFactory connectionFactory ;
+//	
+//	Connection connection;
+//	
+//	Session session;
+//	ThreadLocal<MessageProducer> threadLocal = new ThreadLocal<MessageProducer>();
+//	
+//	public void init(){
+//		try {
+//			connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEN_UEL);
+//			connection = connectionFactory.createConnection();
+//			connection.start();
+//			session = connection.createSession(true, Session.SESSION_TRANSACTED);
+//		} catch (JMSException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	
+//	public void sendMessage(String disname){
+//		try {
+//			Queue queue = session.createQueue(disname);
+//			MessageProducer messageProducer = null;
+//			if (threadLocal.get()!=null) {
+//				messageProducer = threadLocal.get();
+//				
+//			}else {
+//				messageProducer = session.createProducer(queue);
+//				threadLocal.set(messageProducer);
+//				
+//			}
+//			while (true) {
+//				Thread.sleep(1000);
+//				int num = count.getAndIncrement();
+//				//创建一条消息
+////				TextMessage message = session.createTextMessage(Thread.currentThread().getName()+"productor:我正在生产东西，count:"+num);
+//				System.out.println(Thread.currentThread().getName());
+////				messageProducer.send(message);
+//				session.commit();
+//			}
+//		} catch (JMSException | InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//		//消息生产者
+//		
+//	}
+//	
 	
-	AtomicInteger count = new AtomicInteger(0);
-	
-	ConnectionFactory connectionFactory ;
-	
-	Connection connection;
-	
-	Session session;
-	ThreadLocal<MessageProducer> threadLocal = new ThreadLocal<MessageProducer>();
-	
-	public void init(){
-		try {
-			connectionFactory = new ActiveMQConnectionFactory(USERNAME,PASSWORD,BROKEN_UEL);
-			connection = connectionFactory.createConnection();
-			connection.start();
-			session = connection.createSession(true, Session.SESSION_TRANSACTED);
-		} catch (JMSException e) {
-			e.printStackTrace();
+	public static void main(String[] args) throws Exception {
+		ConnectionFactory factory = new ActiveMQConnectionFactory("tcp://localhost:61616");
+		Connection connection = factory.createConnection();
+		connection.start();
+		Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+		Destination destination = session.createQueue("my-queue");
+		MessageProducer producer = session.createProducer(destination);
+		int i = 0;
+		while (i<3) {
+			i++;
+			MapMessage message = session.createMapMessage();
+			message.setBooleanProperty("property"+i, true);
+			message.setBoolean("message"+i, false);
+			producer.send(message);
+			
 		}
-	}
-	
-	
-	public void sendMessage(String disname){
-		try {
-			Queue queue = session.createQueue(disname);
-			MessageProducer messageProducer = null;
-			if (threadLocal.get()!=null) {
-				messageProducer = threadLocal.get();
-				
-			}else {
-				messageProducer = session.createProducer(queue);
-				threadLocal.set(messageProducer);
-				
-			}
-			while (true) {
-				Thread.sleep(1000);
-				int num = count.getAndIncrement();
-				//创建一条消息
-				TextMessage message = session.createTextMessage(Thread.currentThread().getName()+"productor:我正在生产东西，count:"+num);
-				System.out.println(Thread.currentThread().getName());
-				messageProducer.send(message);
-				session.commit();
-			}
-		} catch (JMSException | InterruptedException e) {
-			e.printStackTrace();
-		}
-		//消息生产者
-		
+		session.commit();
+		session.close();
+		connection.close();
 	}
 }
